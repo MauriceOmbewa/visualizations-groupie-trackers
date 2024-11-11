e253ddeeba9ff92fc54f76de24acb7a0040bfafe
mombewa <ombewamaurice79@gmail.com>
2024-11-11 14:28:14 +0300

    updated geocode.go

--- internal/utilities/geocode.go
+++ internal/utilities/geocode.go
@@ -13,10 +13,10 @@ type GeocodeResult struct {
 	Longitude float64 `json:"lng"`
 }
 
-// GeocodeLocation takes a formatted location and fetches the geocode data
-func GeocodeLocation(location string) (GeocodeResult, error) {
+// GeocodeLocation takes a URL, location, and fetches the geocode data
+func GeocodeLocation(apiURL, location string) (GeocodeResult, error) {
 	apiKey := "pk.eyJ1IjoiYXRob29oIiwiYSI6ImNtMWY2N3prZjJsN3MybHNjMWd3bThzOXcifQ.HNgAHQBkzGdrnuS1MtwYlQ" // Replace with your actual API key
-	url := fmt.Sprintf("https://api.geocoding.com/geocode?location=%s&key=%s", location, apiKey)
+	url := fmt.Sprintf("%s?location=%s&key=%s", apiURL, location, apiKey)
 
 	resp, err := http.Get(url)
 	if err != nil {
